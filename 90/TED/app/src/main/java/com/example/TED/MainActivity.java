2a3979f77a5ec9c9969fcf1d8e0ccd105b7c8c8c
trung1122-gif <71000936+trung1122-gif@users.noreply.github.com>
2020-10-15 14:15:49 +0700

    upload

--- 90/TED/app/src/main/java/com/example/TED/MainActivity.java
+++ 90/TED/app/src/main/java/com/example/TED/MainActivity.java
@@ -2,20 +2,49 @@ package com.example.ted;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Adapter;
+import android.widget.GridView;
 
+import com.example.ted.adapter.postersAdapter;
+import com.example.ted.object.posters;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+GridView listPosters;
+postersAdapter adapter;
+ArrayList<posters> postersArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.podcast);
+        setContentView(R.layout.activity_main);
+        init();
+        anhXa();
+        Setup();
+        SetClick();
     }
-    public void batdau(View view) {
-        Intent i = new Intent(this,Worklife.class);
-        startActivity(i);
+
+    private void init(){
+        postersArrayList = new ArrayList<>();
+        postersArrayList.add(new posters("WorkLife with Adam","15 May","https://pi.tedcdn.com/r/pb-assets.tedcdn.com/system/baubles/files/000/007/479/original/Worklife_Podcast.png?1552318499?w=320"));
+        postersArrayList.add(new posters("TED Talks Daily","Thursday","https://pi.tedcdn.com/r/pb-assets.tedcdn.com/system/baubles/files/000/007/473/original/TED_Talks_Daily_Podcasts.png?1552317800?w=320"));
+        postersArrayList.add(new posters("The TED Interview","September","https://pi.tedcdn.com/r/pb-assets.tedcdn.com/system/baubles/files/000/007/472/original/The_TED_Interview_Podcasts.png?1552317773?w=320"));
+        postersArrayList.add(new posters("WorkLife with Adam","15 May","https://pi.tedcdn.com/r/pb-assets.tedcdn.com/system/baubles/files/000/007/479/original/Worklife_Podcast.png?1552318499?w=320"));
+        postersArrayList.add(new posters("TED Talks Daily","Thursday","https://pi.tedcdn.com/r/pb-assets.tedcdn.com/system/baubles/files/000/007/473/original/TED_Talks_Daily_Podcasts.png?1552317800?w=320"));
+        postersArrayList.add(new posters("The TED Interview","September","https://pi.tedcdn.com/r/pb-assets.tedcdn.com/system/baubles/files/000/007/472/original/The_TED_Interview_Podcasts.png?1552317773?w=320"));
+
+        adapter= new postersAdapter(this, 0, postersArrayList);
+    }
+
+    private void anhXa(){
+        listPosters = findViewById(R.id.listPosters);
+    }
+    private void Setup(){
+        listPosters.setAdapter(adapter);
+    }
+    private void SetClick(){
+
     }
 }
